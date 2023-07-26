122fbaa75431d0f9f173729ecd6118dccb674303
julimoncada2006 <juanmoro1117@gmail.com>
2023-07-26 17:12:50 -0500

    reproductor de musica sin mas musica

---
+++ js/music-list.js
@@ -0,0 +1,98 @@
+let allMusic = [
+    {
+        name: "Bruno Mars - That's What I Like",
+        artist: "Bruno Mars",
+        img: "artworks-000221147981-fsq7um-t500x500",
+        src: "That's What I Like Bruno Mars [Official Music Video]"
+    },
+    {
+        name: "Imagine Dragons - Believer",
+        artist: "Imagine Dragons",
+        img: "ab67616d0000b2735675e83f707f1d7271e5cf8a",
+        src: "Believer Imagine Dragons   (Official Music Video)"
+    },
+    {
+        name: "Morat, Juanes - 506",
+        artist: "Morat",
+        img: "morat-y-juanes-506_40600909_20220902073720",
+        src: "506 Morat, Juanes"
+    },
+    {
+        name: "Coldplay - Viva La Vida",
+        artist: "Coldplay",
+        img: "Coldplay-17-10-08",
+        src: "Viva La Vida Coldplay   (Official Video)"
+    },
+    {
+        name: "Drake - God's Plan",
+        artist: "Drake",
+        img: "artworks-000463468011-ws8yh6-t500x500",
+        src: "God's Plan Drake"
+    },
+    {
+        name: "Stray Kids - Youtiful",
+        artist: "Stray Kids",
+        img: "FwqSeGIaUAA6u3L",
+        src: "Youtiful Stray Kids"
+    },
+    {
+        name: "My Chemical Romance - Dead!",
+        artist: "My Chemical Romance",
+        img: "maxresdefault",
+        src: "Dead! My Chemical Romance"
+    },
+    {
+        name: "Brent Faiyaz - Jackie Brown",
+        artist: "Brent Faiyaz",
+        img: "5da925413ac891f42403ceb6163d16a2.1000x1000x1",
+        src: "JACKIE BROWN Brent Faiyaz   [Official Audio]"
+    },
+    {
+        name: "Tavo The Producer - Negro Cirilo",
+        artist: "Tavo The Producer",
+        img: "1200x1200bf-60",
+        src: "NEGRO CIRILO (REMIX)   Prod By TAVO THE PRODUCER"
+    },
+    {
+        name: "Birdy - Not About Angels",
+        artist: "Birdy",
+        img: "not_about_angels-portada",
+        src: "Not About Angels Birdy   (from The Fault In Our Stars Soundtrack) [Official Video]"
+    },
+    {
+        name: "Rata Blanca - La Leyenda Del Hada Y El Mago",
+        artist: "Rata Blanca",
+        img: "ab67616d0000b273478c3f69d4259a077b0d8094",
+        src: "La Leyenda Del Hada Y El Mago Rata Blanca   (Audio)"
+    },
+    {
+        name: "Olivia Rodrigo - Drivers License",
+        artist: "Olivia Rodrigo",
+        img: "artworks-y6gNskVgAg9I0aXa-suEQxg-t500x500",
+        src: "drivers license Olivia Rodrigo   (Official Video)"
+    },
+    {
+        name: "Alex Zurdo X Redimi2 X Funky Ft. Un Corazón, Indiomar, Abby Valdez - A Pesar De Mi",
+        artist: "Redimi2",
+        img: "maxresdefault (1)",
+        src: "A Pesar De Mí   Alex Zurdo x Redimi2 x Funky Ft Un Corazón, Indiomar, Abby Valdez"
+    },
+    {
+        name: "David Bowie - Heroes",
+        artist: "David Bowie",
+        img: "maxresdefault (2)",
+        src: "Heroes David Bowie   (Official Video)"
+    },
+    {
+        name: "Lana Del Rey - Young And Beautiful (DH Orchestal Version)",
+        artist: "Lana Del Rey",
+        img: "maxresdefault (3)",
+        src: "Young and Beautiful (DH Orchestral Version)   Lana Del Rey"
+    },
+    {
+        name: "Papa Roach - She Loves Me Not",
+        artist: "Papa Roach",
+        img: "ab67616d0000b273df35bf65ebd6738a760e2cd1",
+        src: "She Loves Me Not Papa Roach"
+    }
+]
